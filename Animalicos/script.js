4e82ee83f67aa80b4ad630abc22a53d62f83c255
M00nHun7r3ss <116196853+DenisaJuarranzBerindea@users.noreply.github.com>
2024-09-18 18:05:52 +0200

    Add files via upload

---
+++ Animalicos/script.js
@@ -0,0 +1,23 @@
+function PopUpSuricato() {
+  return "Soy un suricato";
+}
+function PopUpArmadillo() {
+	return "Soy un armadillo";
+}
+function PopUpSuricato() {
+	return "Soy un suricato";
+}
+function PopUpSuricato() {
+	return "Soy un suricato";
+}
+function PopUpSuricato() {
+	return "Soy un suricato";
+}
+function PopUpSuricato() {
+	return "Soy un suricato";
+}
+
+function desaparece(nombre) {
+	var button = document.getElementById(nombre);
+  button.style.visibility='hidden';
+}
